475048eb6854455bde25978960d4587dc6eaef43
Alan Pereira Cavalcante <134745847+alancavalcante-dev@users.noreply.github.com>
2025-03-01 15:59:43 -0300

    Update settings.py

--- app/settings.py
+++ app/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['*']
 
 CORS_ALLOWED_ORIGINS = [
-    'http://127.0.0.1:8085',  # O domínio de onde você está fazendo a requisição
+    'http://127.0.0.1:8085', 
 ]
 
 
